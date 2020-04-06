99e8db29289da0331c4fad02079fc25b0d265f59
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-04-06 19:41:31 +0300

    add array of pokemons

--- Sources/App/Controllers/PokemonsController.swift
+++ Sources/App/Controllers/PokemonsController.swift
@@ -12,8 +12,12 @@ final class PokemonsController {
         return Pokemon.query(on: req).all()
     }
     
-    func create(_ req: Request) throws -> Future<Pokemon> {
-        let pokemon = try req.content.decode(Pokemon.self)
-        return pokemon.save(on: req)
+    func create(_ req: Request) throws -> Future<Response> {
+        return try req.content.decode([Pokemon].self).map(to: [Pokemon].self, { (pokemons) in
+            for pokemon in pokemons {
+                _ = pokemon.save(on: req)
+            }
+            return pokemons
+        }).transform(to: req.redirect(to: "pokemons"))
     }
 }
